2d671f810cae72a55e1f91166d8176cb9271b595
Sop-X <147946660+Sop-X@users.noreply.github.com>
2026-08-10 21:46:41 -0400

    Update d_matrix.h

--- d_matrix.h
+++ d_matrix.h
@@ -1,4 +1,4 @@
-// EECE 2560: Sudoku Puzzle Part A
+// EECE 2560: Sudoku Puzzle Part B
 // Team: Elizabeth Carpenter, Sophie Xie, Vyrakaknyka Sambo, Seth Brown
 // This file was provided by the course instructor. It contains the matrix
 // template class used to store the Sudoku board and conflict matrices. 
